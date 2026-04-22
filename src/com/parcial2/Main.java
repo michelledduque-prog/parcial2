09e9ed8a4b9f8f2c392c3e3d8d47398d5cf9e453
Ana Solarte <anasolarte2696@gmail.com>
2026-04-21 20:41:56 -0500

    Se hicieron modificaciones para enriquecer el juego

--- src/com/parcial2/Main.java
+++ src/com/parcial2/Main.java
@@ -1,27 +1,37 @@
 package com.parcial2;
 
-import com.parcial2.batalla.Simulador;
-import com.parcial2.criatura.*;
-import com.parcial2.equipamiento.Arma;
+import com.parcial2.criatura.Arma;
+import com.parcial2.criatura.Criatura;
+import com.parcial2.criatura.Dragon;
+import com.parcial2.criatura.Mago;
+
+
 
 public class Main {
+public static void main(String[] args) {
+        // Creación de personajes y armas
+        Mago merlin = new Mago("Merlín", 80, 15, "Rayo Solar");
+        Dragon smaug = new Dragon("Smaug", 150, 20, "Rojas");
+        Arma baculo = new Arma("Báculo Arcano", 10);
+        
+        merlin.setArma(baculo); // Composición: Equipar arma
 
-    public static void main(String[] args) {
+        simularBatalla(merlin, smaug);
+    }
 
-        Dragon dragon = new Dragon("Colmillo", 66, 15);
-        Mago mago = new Mago("Sombra", 70, 9);
-        Guerrero guerrero = new Guerrero("Acero", 85, 11);
+    public static void simularBatalla(Criatura c1, Criatura c2) {
+        System.out.println("=== COMIENZA EL DUELO: " + c1.getNombre() + " VS " + c2.getNombre() + " ===");
+        
+        while (c1.estaViva() && c2.estaViva()) {
+            c1.atacar(c2);
+            if (c2.estaViva()) {
+                c2.atacar(c1);
+            }
+            System.out.println("-----------------------------------");
+        }
 
-        Arma espada = new Arma("Espada", 10, 4);
-        Arma baston = new Arma("Bastón", 6, 2);
-
-        dragon.equiparArma(espada);
-        guerrero.equiparArma(baston);
-
-        mago.aprenderHechizo();
-        dragon.aprenderHechizo();
-
-        Simulador.simularBatalla(dragon, mago);
-        Simulador.simularBatalla(guerrero, mago);
+        System.out.println("\n🏁 FIN DEL COMBATE");
+        String ganador = c1.estaViva() ? c1.getNombre() : c2.getNombre();
+        System.out.println(" ¡EL GANADOR ES " + ganador.toUpperCase() + "!");
     }
 }
--- src/com/parcial2/criatura/Criatura.java
+++ src/com/parcial2/criatura/Criatura.java
@@ -1,41 +1,27 @@
 package com.parcial2.criatura;
 
-import com.parcial2.equipamiento.Arma;
-
 public abstract class Criatura {
 
-    private final String nombre;
-    private int salud;
-    private final int fuerza;
-    private Arma arma;
+    protected String nombre;
+    protected int salud;
+    protected int fuerza;
 
-    public Criatura(String nombre, int salud, int fuerza) {
+    protected Criatura(String nombre, int salud, int fuerza) {
         this.nombre = nombre;
         this.salud = salud;
         this.fuerza = fuerza;
     }
 
-    public String getNombre() { return nombre; }
-    public int getFuerza() { return fuerza; }
-    public int getSalud() { return salud; }
-    public boolean estaViva() { return salud > 0; }
-
-    public void equiparArma(Arma arma) {
-        this.arma = arma;
-    }
-
-    public Arma getArma() {
-        return arma;
-    }
-
-    public int calcularDañoBase() {
-        return fuerza + (arma != null ? arma.getDañoAdicional() : 0);
-    }
-
-    public void recibirDaño(int daño) {
-        this.salud -= daño;
-    }
-
+    // Métodos abstractos definidos en el Requisito 1
     public abstract void atacar(Criatura objetivo);
     public abstract void defender(int daño);
+
+    // Método concreto para verificar estado de salud
+    public boolean estaViva() {
+        return this.salud > 0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
